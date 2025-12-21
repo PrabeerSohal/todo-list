8464cbdb4c09401700987a2cbf7fa815fd8f52c4
PrabeeSohal <prabeersohal@gmail.com>
2025-12-20 19:52:15 -0500

    added comment

---
+++ todo_list.py
@@ -0,0 +1,18 @@
+To do list in python
+
+Stage 1 ( 1 day):
+Terminal based
+Options:
+
+Create a task
+
+Each task should have:
+Description, time, date, status
+
+Delete a task
+
+Update a task
+Update anything in the task
+
+Storage: 
+store tasks in a file 
